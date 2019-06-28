f5a3603e42ea7ca13a994fc22743c65a1affc25f
Thomas Weise <tweise@hfuu.edu.cn>
2019-06-28 16:36:35 +0800

    minor improvements

--- examples/local_config.R
+++ examples/local_config.R
@@ -14,11 +14,20 @@ repeat {
   dir <- dirname(dir);
   if((dir == "/") || (nchar(dir) <= 1L)) { break; }
 }
+rm("dir");
+rm("subdir");
 
 stopifnot(!is.null(dir.results), nchar(dir.results) > 0L);
+dir.results <- normalizePath(dir.results, mustWork = TRUE);
+stopifnot(dir.exists(dir.results));
 
 dir.evaluation <- file.path(dir.results, "..", "evaluation");
+dir.evaluation <- normalizePath(dir.evaluation, mustWork = TRUE);
+stopifnot(dir.exists(dir.evaluation));
 
 config <- aitoa.config(dir.results = dir.results,
                        dir.evaluation = dir.evaluation,
                        graphics.ext = "eps")
+
+rm("dir.results");
+rm("dir.evaluation");
